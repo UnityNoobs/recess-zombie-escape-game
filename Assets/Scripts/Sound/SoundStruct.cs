b65030c7ef7ecac28871ff7cb57dd5e03f8c18d8
NerdyMrSimms <bill@sutureaudio.com>
2018-11-06 23:54:34 -0500

    FMOD in
    FMOD Package installed in project
    Fmod studio project tied to unity project
    Added scripts that constructs and plays sound effects

---
+++ Assets/Scripts/Sound/SoundStruct.cs
@@ -0,0 +1,17 @@
+﻿public class SoundStruct // this class creates the container for the sound
+{
+    public SoundStruct(FMOD.Studio.EventInstance sound, string name) // constructor
+    {
+        this.name = name;
+        this.instance = sound;
+    }
+    public FMOD.Studio.EventInstance instance { get; private set; } // generating the FMOD Sound in Unity
+    public string name { get; private set; } // give the FMOD instance a name and path reflected from the FMOD Project
+
+    public void modifyFloat(string parameterName, float parameterValue) // This function is called in other scripts to have the sound's parameters changed 
+    {
+        FMOD.Studio.ParameterInstance parameter; // create the container for the parameter 
+        instance.getParameter(parameterName, out parameter); // links the FMOD parameter to unity
+        parameter.setValue(parameterValue); // the parameter's value is changed here
+    }
+}

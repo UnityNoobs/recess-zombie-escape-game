983c266feb249f837103f25314ec3ea63647933c
Brad Hill <Brad-Hill@hotmail.com.au>
2018-11-09 20:04:20 +1100

    Added Playermovement, aslo included zombie horde in this Commit because I used it in an example scene Signed-off-by: Brad Hill <Brad-Hill@hotmail.com.au>

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [Header("Platform options")]
+
+    public float runSpeed = 4f;
+    public float maxRunSpeed = 10f;
+    public float jumpVelocity = 6f;
+    public float jumpMultiplier = 2f;
+    public float fallMultiplier = 2.5f;
+
+    //[Header("Player controls")]
+
+    //public KeyCode keyJump;
+    //public KeyCode keyMoveLeft;
+    //public KeyCode keyMoveRight;
+
+    private Rigidbody2D playerRb;
+    private bool jump = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleInput();
+    }
+
+    private void UpdateJumpVelocity(float multiplier)
+    {
+        playerRb.velocity += Vector2.up * Physics2D.gravity.y * (multiplier - 1) * Time.deltaTime;
+    }
+
+    private void HandleInput()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector2.right * runSpeed * hAxis);
+        if(Mathf.Abs(playerRb.velocity.x) > maxRunSpeed) { playerRb.velocity = new Vector2(Mathf.Sign(playerRb.velocity.x) * maxRunSpeed, playerRb.velocity.y); }
+        // Left movement behavior 
+        /*if (Input.GetKey(keyMoveLeft))
+        {
+            playerRb.AddForce(Vector2.left * runSpeed);
+        }
+
+        // Right movement behavior
+        if (Input.GetKey(keyMoveRight))
+        {
+            playerRb.AddForce(Vector2.right * runSpeed);
+        }
+        */
+        // Jump behavior
+
+        if (playerRb.velocity.y == 0 && Input.GetAxis("Jump") !=0)
+        {
+
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpVelocity);
+            jump = true;
+        }
+
+        if (playerRb.velocity.y < 0)
+        {
+            UpdateJumpVelocity(fallMultiplier);
+        }
+        else if (playerRb.velocity.y > 0 && Input.GetAxis("Jump") == 0)
+        {
+            UpdateJumpVelocity(jumpMultiplier);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground" && jump)
+        {
+            Debug.Log("Landed");
+            jump = false;
+            //if (Input.GetAxis("Horizontal") == 0) { playerRb.velocity = new Vector2(0, 0); }
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieHorde.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHorde : MonoBehaviour {
+    //Speed options for level designers
+    [Header("Horde Speed Settings")]
+    public float hordeSpeed = 0.5f;
+    public float hordeBoostModifier = 1.5f;
+    public float hordeBoostDuration = 2000f;
+    //Horde options for level designers
+    [Header("Horde Options")]
+    [Tooltip("Horde will speed up briefly if it 'eats' an enemy")]
+    public bool canEatEnemies = true;
+    [Tooltip("Will destory level geometry on contact -Needs a level layer-")]
+    public bool canDestroyLevel = false;
+    [Tooltip("Touching the horde means an instant kill")]
+    public bool instantKill = false;
+
+    private Rigidbody2D rb;
+    //The Actual Speed is the speed after modifiers are accounted for
+    private float hordeActualSpeed;
+    private float boostTimer = 0f;
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        hordeActualSpeed = hordeSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Move Right
+        transform.Translate(new Vector3(hordeActualSpeed * Time.deltaTime, 0));
+	}
+    private IEnumerator Boost()
+    {
+        boostTimer = hordeBoostDuration;
+        while(boostTimer != 0)
+        {
+            hordeActualSpeed = hordeSpeed * hordeBoostModifier;
+            boostTimer--;
+            yield return null;
+        }
+        hordeActualSpeed = hordeSpeed;
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("The Zombie Horde has reached the player ");
+            if (instantKill)
+            {
+                Destroy(collision.gameObject);
+            }
+            //TODO: Call Game Controller Player Hits Horde behaviour
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("The Zombie Horde eaten an enemy");
+            if (canEatEnemies)
+            {
+                StartCoroutine("Boost");
+                Destroy(collision.gameObject);
+            }
+        }
+        //This code needs a level layer to be added.
+        /*
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Level"))
+        {
+            Debug.Log("The Zombie Horde Eats a part of the map");
+            if (canDestroyLevel) { Destroy(collision.gameObject); }
+        }
+        */
+    }
+}

351615098ab4f0d9fa985f4ea5f73ff9195156f7
btzr-io <btzr.io@gmail.com>
2018-11-10 00:09:05 -0700

    Add bullet script

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BulletDirection { Left, Right }
+
+public class BulletMovement : MonoBehaviour {
+    public float velocity;
+    public BulletDirection bulletDirection;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // We don't need this...
+    public float GetDirection(BulletDirection direction)
+    {
+        if (direction == BulletDirection.Left)
+        {
+            return -1f;
+        }
+        if (direction == BulletDirection.Right)
+        {
+            return 1f;
+        }
+        return 0f;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float direction = GetDirection(bulletDirection);
+        rb.AddForce((transform.right * direction) * velocity * Time.deltaTime, ForceMode2D.Impulse);
+    }
+}

101a55e777a847a0bc09ac4f8110e031b0479708
Brad Hill <Brad-Hill@hotmail.com.au>
2018-11-15 20:51:18 +1100

    Added Bullet Variation WIP
    Signed-off-by: Brad Hill <Brad-Hill@hotmail.com.au>

---
+++ Assets/Scripts/Weapons/BigWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigWeapon : Weapon {
+
+    public override void Fire(float direction, Transform location)
+    {
+        //base.Fire(direction, location);
+        GameObject projectile = ObjectPool.instance.SpawnFromPool("BigBullets", location.position, location.rotation);
+        projectile.GetComponent<BulletMovement>().direction = direction;
+    }
+}

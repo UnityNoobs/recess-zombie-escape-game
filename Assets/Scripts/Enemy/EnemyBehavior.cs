30af8249b4e2c5624208429934506e953421090f
btzr-io <btzr.io@gmail.com>
2018-11-15 21:17:47 -0700

    improve enemy logic and implement enemy state script

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -2,45 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class EnemyBehavior : MonoBehaviour {
-    [Header("Follow options")]
-    public float speed = 10f;
-    public float distance = 1f;
-    public string tagName = "Player";
-    
-    private float direction = 0f;
-    private Transform target;
+
+    [Header("Life")]
+    public float health = 10f;
+
     private Rigidbody2D rb;
-    
-    // Flags
-    private bool canMove = true;
+    private EnemyState state;
 
-
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag(tagName).transform;
+        state = GetComponent<EnemyState>();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // Follow player on axis x
-        if(Vector2.Distance(transform.position, target.position) > distance && canMove)
-        {
-            direction = GetVelocityDirection();
-            rb.velocity = Vector2.right * direction * speed * Time.deltaTime;
-        }
+    public void Start()
+    {
+        state.SetState("follow");
     }
 
     public void HandleImpact(float impactDirection, float impactForce)
     {
-        canMove = false;
-        StartCoroutine(DisasbleMovement(1f, impactDirection, impactForce));
-
+        if(!state.CompareState("dead"))
+        {
+            StartCoroutine(DisasbleMovement(1f, impactDirection, impactForce));
+            HandleDamage(impactForce);
+            Debug.Log("Collision!");
+        }
     }
 
-    // Utils
+    private void HandleDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0) TriggerDeath();   
+    }
+
+    private void TriggerDeath()
+    {
+        StartCoroutine(DisableEnemy());
+    }
 
     private void PushBack(float pushDirection, float force)
     {
@@ -49,24 +51,24 @@ public class EnemyBehavior : MonoBehaviour {
 
     private IEnumerator DisasbleMovement(float waitTime, float impactDirection, float impactForce)
     {
-        canMove = false;
-        // Push-back object
+        // Push enemy on bullet direction
+        state.SetState("freeze");
         PushBack(impactDirection, impactForce);
+        // Wait some time before enable the enemy movement
         yield return new WaitForSeconds(waitTime);
-        canMove = true;
-        yield break;
- 
+        state.SetState("follow");
+        // Exit courutine
+        yield return null;
     }
 
-    private float GetVelocityDirection()
+    private IEnumerator DisableEnemy()
     {
-        float x = transform.position.x;
-        float targetX = target.position.x;
-
-        if (x==targetX)
-        {
-            return 0f;
-        } 
-        return x < target.position.x ? 1 : -1;
+        // Push enemy on bullet direction
+        state.SetState("dead");
+        // Wait some time before destroy the enemy
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+        // Exit courutine
+        yield return null;
     }
 }

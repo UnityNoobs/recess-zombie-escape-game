3c019fe5e11bdba80bbf07e3fab651407aca6845
jbouzy-laptop <joshbouzy@gmail.com>
2018-11-28 21:04:40 -0500

    Spike Trap and Falling Trap Prefabs and Scripts
    The Spike Trap, similar to the Enemy Attack script, will kill the player object upon collision. The falling platform trap will drop after one second once collision with the player object is detected.

---
+++ Assets/Scripts/Traps/SpikeTrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+
+    public float attackDamage = 100;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            TriggerKillPlayer(collision.gameObject.GetComponent<PlayerHealth>()); //Accesses 
+        }
+    }
+
+    public void TriggerKillPlayer(PlayerHealth player)
+    {
+        player.HandleDamage(attackDamage);
+    }
+}
